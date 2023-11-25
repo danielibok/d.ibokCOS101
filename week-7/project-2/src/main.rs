d1bbd6a68c0737a93f4ad194386e9cbc5a77c8fd
danielibok <daniel.ibok@pau.edu.ng>
2023-11-25 15:59:19 +0100

    my seventh commit

---
+++ week-7/project-2/src/main.rs
@@ -0,0 +1,17 @@
+// Rust program that can ask the number of siblings of a client.
+
+use std::io;
+
+fn main() {
+ let mut number_of_siblings = String::new();
+ io::stdin().read_line(&mut number_of_siblings).expect("Failed to read input");
+ let number_of_siblings:f64 = number_of_siblings.trim().parse().expect("Invalid number of siblings");
+ 
+ let mut first_name = String::new();
+
+ let mut age = String::new();
+ let mut waec_status = String::new();
+
+
+    println!("");
+}
